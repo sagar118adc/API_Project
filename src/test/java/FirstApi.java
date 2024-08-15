e85cc49708c3f56fe53bf440af5d40989184f7f0
unknown <sagar118adc@gmail.com>
2024-08-15 22:12:15 +0530

    Added post method

--- src/test/java/FirstApi.java
+++ src/test/java/FirstApi.java
@@ -1,6 +1,8 @@
+import jdk.jfr.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class FirstApi {
 
@@ -12,5 +14,19 @@ public class FirstApi {
                 .then()
                 .assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    public void postMethod() {
+        String jsonBody ="{\"name\":\"sagar\", \"job\":\"QA\"}";
+        given()
+                .header("content-type", "application/json")
+                .body(jsonBody)
+                .when().post("https://reqres.in/api/users")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .and()
+                .body("name", equalTo("sagar"));
     }
 }
