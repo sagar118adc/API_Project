763da8a0d883a951395d85bba718c8685624f5df
unknown <sagar118adc@gmail.com>
2024-08-20 16:58:57 +0530

    modified tesng and APITESTING class files

--- src/test/java/APITESTING.java
+++ src/test/java/APITESTING.java
@@ -1,4 +1,4 @@
-import jdk.security.jarsigner.JarSigner;
+import io.restassured.RestAssured;
 import org.json.JSONObject;
 import org.testng.annotations.*;
 
@@ -6,6 +6,12 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class APITESTING {
+
+
+  @BeforeTest
+  public void beforeTest() {
+    baseURI="https://reqres.in/api";
+  }
 
   @Test
   public void jObject() {
@@ -16,11 +22,21 @@ public class APITESTING {
         .header("content-type", "application/json")
         .body(jsonObject.toString())
         .when()
-        .post("https://reqres.in/api/users")
+        .post("/users")
         .then()
         .statusCode(201)
         .and()
         .body("name", equalTo("Rajesh"));
 
+  }
+
+  @Test
+  public void getMethod(){
+    given()
+        .when()
+        .get("/users?page=2")
+        .then()
+        .assertThat()
+        .statusCode(200);
   }
 }
